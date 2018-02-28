98e85b99efc220f0850ec2d60a510373ab7b4ce5
MaxXSoft <x@MaxXSoft.net>
2018-02-28 10:15:02 +0800

    (player.js) implemented the volume-related part

--- html/script/player.js
+++ html/script/player.js
@@ -2,14 +2,24 @@ var Player = function() {
     var p = {
         paused: false,
         seeking: false,
+        changingVolume: false,
+        mouseInVolumeBox: false,
     }
 
     p.playButton = $('play-button')
     p.controller = $('music-control')
+
+    p.progressBar = $('progress-bar')
     p.progress = $('progress')
     p.handle = $('handle')
     p.timeCode = $('time-code')
-    p.total = $('progress-bar').offsetWidth
+    p.total = p.progressBar.offsetWidth
+
+    p.volumeButton = $('volume-button')
+    p.volumeBox = $('volume-container')
+    p.volumeBar = $('volume-bar')
+    p.volume = $('volume')
+    p.volumeHandle = $('volume-handle')
 
     p.showButtonImage = function() {
         var imageUrl = ''
@@ -113,6 +123,9 @@ var Player = function() {
             p.startUpdateLoop()
         }, 500)
     }
+    p.seek = function(percent) {
+        p.controller.currentTime = p.controller.duration * percent
+    }
 
     p.handle.onmousedown = function(e) {
         p.seeking = true
@@ -122,8 +135,7 @@ var Player = function() {
     p.handle.onmouseup = function(e) {
         if (p.seeking) {
             p.seeking = false
-            var percent = p.progress.offsetWidth / p.total
-            p.controller.currentTime = p.controller.duration * percent
+            p.seek(p.progress.offsetWidth / p.total)
         }
     }
     p.handle.onmousemove = function(e) {
@@ -131,13 +143,112 @@ var Player = function() {
             p.moveProgress(p.handleX + e.clientX - p.mouseX)
         }
     }
-    document.body.onmousemove = p.handle.onmousemove
-    document.body.onmouseup = p.handle.onmouseup
+    p.progressBar.onmouseup = function(e) {
+        p.seek((e.clientX - p.progressBar.getBoundingClientRect().left) / p.total)
+    }
+    p.progress.onmouseup = p.progressBar.onmouseup
+    document.body.addEventListener('mousemove', p.handle.onmousemove)
+    document.body.addEventListener('mouseup', p.handle.onmouseup)
 
     window.addEventListener('resize', function() {
         p.total = $('progress-bar').offsetWidth
         p.updateProgress()
     })
+
+    p.setVolume = function(percent) {
+        p.controller.volume = percent
+    }
+    p.updateVolumeIcon = function(percent) {
+        var levels = [
+            "url('pic/volume0.svg')",
+            ", url('pic/volume1.svg')",
+            ", url('pic/volume2.svg')",
+            ", url('pic/volume3.svg')",
+        ]
+        var bg
+        if (percent < 0.25) {
+            bg = levels[0]
+        }
+        else if (percent < 0.5) {
+            bg = levels[0] + levels[1]
+        }
+        else if (percent < 0.75) {
+            bg = levels[0] + levels[1] + levels[2]
+        }
+        else {
+            bg = levels[0] + levels[1] + levels[2] + levels[3]
+        }
+        p.volumeButton.style.backgroundImage = bg
+    }
+    p.moveVolume = function(width) {
+        if (width > p.volumeBar.offsetWidth) {
+            width = parseInt(p.volumeBar.offsetWidth)
+        }
+        else if (width < 0) {
+            width = parseInt(0)
+        }
+        else {
+            width = parseInt(width)
+        }
+        p.volume.style.width = width + 'px'
+        p.volumeHandle.style.left = width + 'px'
+        p.updateVolumeIcon(width / p.volumeBar.offsetWidth)
+    }
+    p.volumeButton.onmouseover = function() {
+        p.volumeBox.style.opacity = '1'
+    }
+    p.volumeButton.onmouseout = function() {
+        if (!p.changingVolume) {
+            setTimeout(function() {
+                if (!p.mouseInVolumeBox) {
+                    p.volumeBox.style.opacity = '0'
+                }
+            }, 100)
+        }
+    }
+    p.volumeBox.onmouseenter = function() {
+        p.mouseInVolumeBox = true
+    }
+    p.volumeBox.onmouseleave = function() {
+        p.mouseInVolumeBox = false
+        if (p.changingVolume) {
+            var id = setInterval(function() {
+                if (!p.changingVolume && !p.mouseInVolumeBox) {
+                    p.volumeBox.style.opacity = '0'
+                    clearInterval(id)
+                }
+            }, 100)
+        }
+        else {
+            p.volumeBox.style.opacity = '0'
+        }
+    }
+    p.volumeHandle.onmousedown = function(e) {
+        p.changingVolume = true
+        p.handleX = p.volumeHandle.offsetLeft
+        p.mouseX = e.clientX
+    }
+    p.volumeHandle.onmouseup = function(e) {
+        if (p.changingVolume) {
+            p.changingVolume = false
+        }
+    }
+    p.volumeHandle.onmousemove = function(e) {
+        if (p.changingVolume) {
+            p.moveVolume(p.handleX + e.clientX - p.mouseX)
+            p.setVolume(p.volume.offsetWidth / p.volumeBar.offsetWidth)
+        }
+    }
+    p.volumeBar.onmouseup = function(e) {
+        var width = e.clientX - p.volumeBar.getBoundingClientRect().left
+        p.moveVolume(width)
+        p.setVolume(width / p.volumeBar.offsetWidth)
+    }
+    p.volume.onmouseup = p.volumeBar.onmouseup
+    document.body.addEventListener('mousemove', p.volumeHandle.onmousemove)
+    document.body.addEventListener('mouseup', p.volumeHandle.onmouseup)
+    p.moveVolume(p.volumeBar.offsetWidth)
+    p.setVolume(1)
 
     return p
 }
