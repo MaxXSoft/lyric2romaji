09af058c35ebb39aadf672c38cd094eb7c1bf1b3
MaxXSoft <x@MaxXSoft.net>
2018-03-01 22:28:24 +0800

    (main.js) modified some details

--- html/script/main.js
+++ html/script/main.js
@@ -3,6 +3,7 @@ var applyInfo = function(player) {
         albumPicUrl = "url('" + json.album.pic + "')"
         player.setAlbumCover(albumPicUrl)
         player.setSongInfo(json.name, json.artists, json.album.name)
+        document.title = json.name + ' - [Lyric->Romaji]'
     })
 }
 
